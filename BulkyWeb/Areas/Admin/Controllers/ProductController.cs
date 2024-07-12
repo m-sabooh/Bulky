3a8984ce6f003e83b6ddc52692a50a59548a0fc9
Sabooh <sabooh22280@gmail.com>
2024-07-12 10:18:56 +0300

    Category List in Product Create Page using View Data

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -28,7 +28,8 @@ namespace BulkyWeb.Areas.Admin.Controllers
                 Text = u.Name,
                 Value = u.Id.ToString(),
             });
-            ViewBag.CategoryList = CategoryList;
+            /*ViewBag.CategoryList = CategoryList;*/
+            ViewData["CategoryList"] = CategoryList;
             return View();
         }
         [HttpPost]
